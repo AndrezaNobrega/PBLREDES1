2d0333fac79a85b460ccf64f9ca91b9553dabf7d
AndrezaNobrega <nobregandreza@gmail.com>
2022-09-09 19:05:40 -0300

    Enviando informações para células na planilha

--- v2/servidor/googlesheet.py
+++ v2/servidor/googlesheet.py
@@ -8,14 +8,8 @@ from google.oauth2.credentials import Credentials
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1SDuvwzFTQ4_KIgtYVUgTLKVa2okDRYBc4gXl2Fn8fO0' #ID DA MINHA PLANILHA 
-SAMPLE_RANGE_NAME = 'Página1!A1:B12' #AQUI É O INTERVALO QUE PRECISO PROCURAR
-
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+    #Script para conexão servidor/nuvem (google sheets)
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -28,27 +22,37 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'v2/servidor/client_secret.json', SCOPES)
+                'v2/servidor/client_secret.json', SCOPES) #arquivo usado para fazer a primeira autenticação
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('token.json', 'w') as token: #Autenticação p/ acessar a planilha
             token.write(creds.to_json())
 
-"""     service = build('sheets', 'v4', credentials=creds)
+    service = build('sheets', 'v4', credentials=creds)
 
     # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+    sheet = service.spreadsheets() #resgata o arquivo
+    result = sheet.values().get(spreadsheetId='1SDuvwzFTQ4_KIgtYVUgTLKVa2okDRYBc4gXl2Fn8fO0',
+                                range='Página1!A1:D14').execute()
+    values = result.get('values', []) # pegamos o valor da célula
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
- """
+    print(values)
+    
+    valoresAdicionar = [["12345", "258", '2022-09-07 19:02', '89'],
+                        ["12345", "258", '2022-09-07 19:02', '89'],
+                        ["12345", "258", '2022-09-07 19:02', '89'],
+                        ["12345", "258", '2022-09-07 19:02', '89'],
+                            ]
+
+    result = sheet.values().update(spreadsheetId='1SDuvwzFTQ4_KIgtYVUgTLKVa2okDRYBc4gXl2Fn8fO0',
+                                   range='Página1!A1', 
+                                   valueInputOption= 'USER_ENTERED', 
+                                   body= {"values": valoresAdicionar}).execute()
+    
+
+
+#ir add de 10s em 10
+
+    
 if __name__ == '__main__':
     main()
--- v2/servidor/nuvem.py
+++ v2/servidor/nuvem.py
@@ -1,5 +1,11 @@
+from __future__ import print_function
 import socket
-import json
+from time import sleep
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 60000
@@ -7,19 +13,54 @@ UDP_PORT_NO = 60000
 #criando o server
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) #usamos o IP e a porta
-dadosHidr = [] #criando lista p o json dos dados dos hidrometros
-while True:
-    data, addr = serverSock.recvfrom(1024)    
-    dado = data.decode()
-    consumo = {"id": dado[:-22],
-               "lSomados": dado[-20:-6],
-               "data": dado[-6:-4],
-               "nVazao":dado[-4:]
-    }
-    jsonConsumo = json.dumps(consumo) #criando um Json
-    dadosHidr.append(jsonConsumo)
-    
-    print('\nLitros utilizados: ' + dado[:-22])
-    print('\nHorário/Data: ' + dado[-20:-6])
-    print('\nVazão atual: ' + dado[-6:-4])
-    print('\n ID:' + dado[-4:])
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+def main():
+    #Script para conexão servidor/nuvem (google sheets)
+    creds = None    
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'v2/servidor/client_secret.json', SCOPES) #arquivo usado para fazer a primeira autenticação
+            creds = flow.run_local_server(port=0)
+        # salvamos para o próximo acesso
+        with open('token.json', 'w') as token: #Autenticação p/ acessar a planilha
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    """ #usar para receber
+    sheet = service.spreadsheets() #resgata o arquivo
+    result = sheet.values().get(spreadsheetId='1SDuvwzFTQ4_KIgtYVUgTLKVa2okDRYBc4gXl2Fn8fO0',
+                                range='Página1!A1:D14').execute()
+    values = result.get('values', []) # pegamos o valor da célula
+
+    print(values) """
+
+#recebendo no servidor e enviando para nuvem
+    print('Servidor pronto')
+    dadosHidr = [] #criando lista p os dados dos hidrometros
+    while True:
+        data, addr = serverSock.recvfrom(1024)    
+        dado = data.decode()
+        consumo = [dado[:-22], dado[-20:-6], dado[-6:-4], dado[-4:]]    #separando cada dado para uma célula
+        dadosHidr.append(consumo)
+        #envio para google sheets
+        result = sheet.values().update(spreadsheetId='1SDuvwzFTQ4_KIgtYVUgTLKVa2okDRYBc4gXl2Fn8fO0',
+                                    range='Página1!A2', 
+                                    valueInputOption= 'USER_ENTERED', 
+                                    body= {"values": dadosHidr}).execute()
+        
+        print('\nLitros utilizados: ' + dado[:-22])
+        print('\nHorário/Data: ' + dado[-20:-6])
+        print('\nVazão atual: ' + dado[-6:-4])
+        print('\n ID:' + dado[-4:])
+        sleep(10)
+
+if __name__ == '__main__':
+    main()
